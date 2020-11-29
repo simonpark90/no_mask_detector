5007efba1a6cc0c3cf67e9a7c004089e91382c92
j00hyun <loove1997@naver.com>
2020-11-29 16:25:44 +0900

    rest 통신 및 카메라 구성

--- no_mask_ios/ObjectRecognition/CaptureVideoPreviewView.swift
+++ no_mask_ios/ObjectRecognition/CaptureVideoPreviewView.swift
@@ -10,7 +10,9 @@ import AVFoundation
 import UIKit
 
 class CapturePreviewView: UIView {
+    //뷰를 생성하는 초기에 호출되어 뷰를 인스턴스화 하고 그에 연결할 CALayer를 결정한다.
     override class var layerClass: AnyClass{
+        //동영상 프레임을 처리하기 위한 것이다
         return AVCaptureVideoPreviewLayer.self
     }
 
--- no_mask_ios/ObjectRecognition/ViewController.swift
+++ no_mask_ios/ObjectRecognition/ViewController.swift
@@ -14,14 +14,29 @@ class ViewController: UIViewController {
 
     @IBOutlet var previewView:CapturePreviewView!
     @IBOutlet var classifiedLabel:UILabel!
+    @IBOutlet var adminList:UITextView!
+    
+    @IBAction func get(_sender : Any) {
+        do {
+            // URL 설정 GET 방식으로 호출
+            let url = URL(string: "http://54.180.165.95:3000/list")
+            let response = try String(contentsOf: url!)
+            
+            // 읽어온 값을 레이블에 표시
+            self.adminList.text = response
+        } catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
     
     let videoCapture : VideoCapture = VideoCapture()
-    
 
     let context = CIContext()
     
     let model = MobileNetV2()
 //    let model = Inceptionv3()
+    
+    // 카메라를 초기화 한 후 세션에 할당
     override func viewDidLoad() {
         super.viewDidLoad()
         self.videoCapture.delegate = self
