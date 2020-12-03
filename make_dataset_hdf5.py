d13af64193e61dd85534c8ae476a69d841878830
simonpark90 <simonswpark90@gmail.com>
2020-12-03 15:51:50 +0900

    add preprocessing and make_dataset

---
+++ make_dataset_hdf5.py
@@ -0,0 +1,87 @@
+import numpy as np
+import tables 
+import glob
+import cv2
+
+from random import shuffle
+
+shuffle_data = True
+hdf5_path = './image.hdf5'
+data_path = './data/*/*/*/*.jpg'
+
+addrs = glob.glob(data_path)
+
+labels = [0 if 'YES' in addr else 1 for addr in addrs]
+
+if shuffle_data:
+    c = list(zip(addrs, labels))
+    shuffle(c)
+    addrs, labels = zip(*c)
+
+
+train_addrs = addrs[0:int(0.6*(len(addrs)))]
+train_labels = labels[0:int(0.6*(len(labels)))]
+
+val_addrs = addrs[int(0.6*(len(addrs))):int(0.8*(len(addrs)))]
+val_labels = labels[int(0.6*(len(labels))):int(0.8*(len(labels)))]
+
+test_addrs = addrs[int(0.8*(len(addrs))):]
+test_labels = labels[int(0.8*(len(labels))):]
+
+img_dtype = tables.UInt8Atom()
+
+data_shape = (0,224,224,3)
+
+hdf5_file = tables.open_file(hdf5_path, mode = 'w')
+
+train_storage = hdf5_file.create_earray(hdf5_file.root, 'train_img', img_dtype, shape = data_shape)
+val_storage = hdf5_file.create_earray(hdf5_file.root, 'val_img', img_dtype, shape = data_shape)
+test_storage = hdf5_file.create_earray(hdf5_file.root, 'test_img', img_dtype, shape = data_shape)
+
+mean_storage = hdf5_file.create_earray(hdf5_file.root, 'train_mean', img_dtype, shape=data_shape)
+
+
+hdf5_file.create_array(hdf5_file.root, 'train_labels', train_labels)
+hdf5_file.create_array(hdf5_file.root, 'val_labels', val_labels)
+hdf5_file.create_array(hdf5_file.root, 'test_labels', test_labels)
+
+mean = np.zeros(data_shape[1:], np.float32)
+
+for i in range(len(train_addrs)):
+    if i % 1000 == 0 and i >1:
+        print('Train data: {}/{}'.format(i,len(train_addrs)))
+
+    addr = train_addrs[i]
+    img = cv2.imread(addr)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    train_storage.append(img[None])
+    mean += img / float(len(train_labels))
+
+for i in range(len(val_addrs)):
+    if i % 1000 == 0 and i > 1:
+        print ('Validation data: {}/{}'.format(i, len(val_addrs)))
+
+    addr = val_addrs[i]
+    img = cv2.imread(addr)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+    val_storage.append(img[None])
+
+for i in range(len(test_addrs)):
+    if i % 1000 == 0 and i > 1:
+        print ('Test data: {}/{}'.format(i, len(test_addrs)))
+
+    addr = test_addrs[i]
+    img = cv2.imread(addr)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+    test_storage.append(img[None])
+
+mean_storage.append(mean[None])
+hdf5_file.close()
--- train_model_from_hdf5.py
+++ train_model_from_hdf5.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import make_model 
+import tables
+
 
 from tensorflow import keras
 from tensorflow.keras import models
@@ -12,52 +14,79 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 
+from random import shuffle
+from math import ceil
 from matplotlib import pyplot as plt
 
 
 '''
     author : Sungwoo Park
-    version : 1.3
+    version : 1.4
     base_model : MobileNet V2
     early_stopping: 50
     optimizers: Adam
     create date : 2020.11.16
-    fixed data : 2020.11.27
+    fixed data : 2020.12.02
 '''
 
 def train(inputs, classes, fine_tune_at):
    
-    # np_load_old = np.load
-    # np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
-    
-    # X_train, X_test, y_train, y_test = np.load('./img_data.npy')
+    hdf5_path = './image.hdf5'
+    subtract_mean = False
     batch_size = 32
-    train_datagen = ImageDataGenerator(
-        rescale = 1./225,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=False,
-        validation_split=0.2)
-    test_datagen = ImageDataGenerator(rescale = 1./255)
 
-    train_generator = train_datagen.flow_from_directory(
-        'data/train',
-        target_size = (224,224),
-        batch_size = batch_size,
-        class_mode = 'categorical',
-        subset = 'training'
-    )
-    validation_generator = train_datagen.flow_from_directory(
-        'data/train',
-        target_size = (224,224),
-        batch_size = batch_size,
-        class_mode = 'categorical',
-        subset = 'validation'
-    )
-    test_generator = test_datagen.flow_from_directory(
-        'data/test',
-        target_size = (224,224)
-    )
+
+    hdf5_file = tables.open_file(hdf5_path, mode = 'r')
+
+    if subtract_mean:
+        mm = hdf5_file.root.train_mean[0]
+        mm = mm[np.newaxis, ...]
+    data_num = hdf5_file.root.train_img.shape[0]
+
+    batch_list = list(range(int(ceil(float(data_num)/batch_size))))
+    shuffle(batch_list)
+
+    for n, i in enumerate(batch_list):
+        i_s = i * batch_size
+        i_e = min([i+1*batch_size,data_num])
+
+        images = hdf5_file.root.train_img[i_s:i_e]
+        if subtract_mean:
+            images -= mm
+
+        labels = hdf5_file.root.train_labels[i_s:i_e]
+        labels_one_hot = np.zeros((batch_size,nb_class))
+        labels_one_hot[np.arange(batch_size),labels] = 1
+
+        print(n+1,'/',len(batch_list))
+
+        print 
+    # train_datagen = ImageDataGenerator(
+    #     rescale = 1./225,
+    #     shear_range=0.2,
+    #     zoom_range=0.2,
+    #     horizontal_flip=False,
+    #     validation_split=0.2)
+    # test_datagen = ImageDataGenerator(rescale = 1./255)
+
+    # train_generator = train_datagen.flow_from_directory(
+    #     'data/train',
+    #     target_size = (224,224),
+    #     batch_size = batch_size,
+    #     class_mode = 'categorical',
+    #     subset = 'training'
+    # )
+    # validation_generator = train_datagen.flow_from_directory(
+    #     'data/train',
+    #     target_size = (224,224),
+    #     batch_size = batch_size,
+    #     class_mode = 'categorical',
+    #     subset = 'validation'
+    # )
+    # test_generator = test_datagen.flow_from_directory(
+    #     'data/test',
+    #     target_size = (224,224)
+    # )
     
     model = make_model.create_model(inputs, classes, fine_tune_at)
 
